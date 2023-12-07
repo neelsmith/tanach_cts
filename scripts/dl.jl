7b474ac9bf910db1f7070ae5e3312f878cde9919
Neel Smith <dnsmith.neel@gmail.com>
2023-12-07 06:24:31 -0500

    Download files from list with correct URI escaping

--- scripts/dl.jl
+++ scripts/dl.jl
@@ -1,5 +1,6 @@
 # Download text files from tanach.us
 using Downloads
+using HTTP
 
 # File locations
 outdir = joinpath(pwd(), "textsrc")
@@ -14,10 +15,10 @@ books = Dict([
     ("Deut", "deuteronomy"),
     ("Josh", "joshua"),
     ("Judg", "judges"),
-    ("1 Sam", "samuel1")#=,
+    ("1 Sam", "samuel1"),
     ("2 Sam", "samuel2"),
-    ("1 Kings", "kings1")
-    ("2 Kings", "kings2")=#
+    ("1 Kings", "kings1"),
+    ("2 Kings", "kings2")
     ]
 )
 
@@ -26,7 +27,7 @@ bklist = keys(books) |> collect
 @info("Looking for $(length(bklist)) books:")
 for b in bklist #keys(books)
     @info("=> Look for key $(b)")
-    url = string(baseurl, b, "*")
+    url = string(baseurl, HTTP.escapeuri(b), "*")
     dlfile = Downloads.download(url)
     @info("Downloaded $(url)")
     outfile = joinpath(outdir, string(books[b], ".txt"))
